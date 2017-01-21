7e7b29bd4689aa45f3003e7b5730df2e804bf3ca
Aaron Neustedter <aaron97neu@yahoo.com>
2017-01-21 17:39:11 -0500

    Added workshop and more cs code

---
+++ New Unity Project/Assets/Dao/Mover.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+    //                     
+    //          INITIAL POSITIONS
+    // 
+    //               ,->[A]
+    //active image  --->[A] [t]
+    //list           '->[A] [t] <-,
+    //                      [t] <---- All image list
+    //                      [t] <
+    //                      [t]
+    //
+    public PicPane mainPane;
+    public GameObject infoPane;
+    public List<ImageModel> allModels;
+    public List<ImageModel> activeModels;
+
+	// Use this for initialization
+	void Start () {
+        //to implement: get list of models
+        mainPane = GameObject.FindWithTag("InfoPane").GetComponent(typeof(PicPane)) as PicPane;
+        infoPane = GameObject.FindWithTag("MainPane");
+        mainPane.setActive();
+        mainPane.loadImage();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void setInitalActiveModels(List<ImageModel> all)
+    {
+       
+
+    }
+
+    void populateActive()
+    {
+        //to be implemented
+
+    }
+
+
+}
--- New Unity Project/Assets/Dao/PicPane.cs
+++ New Unity Project/Assets/Dao/PicPane.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 
 public class PicPane : MonoBehaviour {
 
-// Add this script to a GameObject. The Start() function fetches an
-// image from the documentation site.  It is then applied as the
-// texture on the GameObject.
+    // Add this script to a GameObject. The Start() function fetches an
+    // image from the documentation site.  It is then applied as the
+    // texture on the GameObject.
 
     public string url = "https://docs.unity3d.com/uploads/Main/ShadowIntro.png"; //Will be depreciated as soon as ImageMode is implemented
     public ImageModel myModel;
@@ -19,7 +19,7 @@ public class PicPane : MonoBehaviour {
         
     }
 
-    IEnumerator loadImage()
+    public IEnumerator loadImage()
     {
         Texture2D tex;
         tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
@@ -37,7 +37,14 @@ public class PicPane : MonoBehaviour {
     void setImageModel(ImageModel i)
     {
         myModel = i;
+        url = i.ImageUrl;
         loadImage();
           
+    }
+
+    public void setActive()
+    {
+        GameObject infoPane = GameObject.FindWithTag("InfoPane");
+        //Set all stuff here
     }
 }
