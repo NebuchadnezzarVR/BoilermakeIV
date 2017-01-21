eea06a9bc0037e9326b45f8b309a4845252d9317
Aaron Neustedter <aaron97neu@yahoo.com>
2017-01-21 16:31:27 -0500

    Added detail to picpane.cs

--- New Unity Project/Assets/PicPane.cs
+++ New Unity Project/Assets/PicPane.cs
@@ -9,21 +9,35 @@ public class PicPane : MonoBehaviour {
 // image from the documentation site.  It is then applied as the
 // texture on the GameObject.
 
-    public string url = "https://docs.unity3d.com/uploads/Main/ShadowIntro.png";
-    
-    IEnumerator Start() {
-    
+    public string url = "https://docs.unity3d.com/uploads/Main/ShadowIntro.png"; //Will be obsolete as soon as ImageMode is implemented
+    public ImageModel myModel;
+
+    //Currently incative. Will implement when movement becomes a possibility
+
+
+    void Start() {
+        
+    }
+
+    IEnumerator loadImage()
+    {
         Texture2D tex;
         tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
         WWW www = new WWW(url);
         yield return www;
         www.LoadImageIntoTexture(tex);
         GetComponent<Renderer>().material.mainTexture = tex;
-    }
 
-	
-	// Update is called once per frame
-	void Update () {
+
+    }
+    // Update is called once per frame
+    void Update () {
 		
 	}
+    void setImageModel(ImageModel i)
+    {
+        myModel = i;
+        loadImage();
+          
+    }
 }
