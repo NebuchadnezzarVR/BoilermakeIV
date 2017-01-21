37e4d9206181326763bdea9d9545d37e54ede124
Aaron Neustedter <aaron97neu@yahoo.com>
2017-01-21 16:44:14 -0500

    added Dao

---
+++ New Unity Project/Assets/Dao/ImageModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+	public class ImageModel
+	{
+		public DateTime TimeTaken { get; set; }
+		public string Username { get; set; }
+		public string FullName { get; set; }
+		public List<string> TaggedUsers { get; set; }
+		public List<string> Likers { get; set; }
+		public string ImageUrl { get; set; }
+		public string Caption { get; set; }
+		public int Comments { get; set; }
+		public int Likes { get; set; }
+	}
