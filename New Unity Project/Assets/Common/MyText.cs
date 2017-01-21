7ff3decc867c923979b35411646e8f495fc6b4c0
STARGATE\shawn <shawnkmontgomery@gmail.com>
2017-01-21 18:19:13 -0500

    no message

---
+++ New Unity Project/Assets/Common/MyText.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyText : MonoBehaviour {
+    Text txt;
+    string username = "";
+    int numLikes = 0;
+    int numComments = 0;
+    string caption = "";
+    string[] likes = { "", "", "" };
+
+    // Use this for initialization
+    public void Start()
+    {
+        txt.text = "";
+    }
+
+    public void setUsername(string user)
+    {
+        username = user;
+    }
+
+    public void setLikes(int l)
+    {
+        numLikes = l;
+    }
+
+    public void likeIt(string s)
+    {
+        numLikes++;
+        likes[2] = likes[1];
+        likes[1] = likes[0];
+        likes[0] = s;
+    }
+
+    public void setComments(int c)
+    {
+        numComments = c;
+    }
+
+    public void setCaption(string s)
+    {
+        caption = s;
+    }
+
+    public void likePeople(string[] arr)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            likes[i] = arr[i];
+        }
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        txt.text = "" + username + "\n" + caption + "\n" + "Likes: " + numLikes + "\t Comments: " + numComments + "\n" + likes[0] + likes[1] + likes[2];
+    }
+}
---
+++ New Unity Project/Assets/Common/TextManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MyText txt = GameObject.FindWithTag("SpecialCanvas").GetComponentInChildren(typeof(MyText)) as MyText;
+        txt.setUsername("mrshawnabu");
+        txt.setCaption("Fucking hate this");
+        txt.setLikes(5);
+        string[] arr = { "Jordan", "MyDick", "YourDick" };
+        txt.likePeople(arr);
+        txt.setComments(3);
+        txt.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
