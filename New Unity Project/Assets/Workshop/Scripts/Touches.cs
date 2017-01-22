3eb878fbf1c873ec386e4f8a07c0de292e061987
STARGATE\shawn <shawnkmontgomery@gmail.com>
2017-01-22 01:21:24 -0500

    Working on the Dpad controls

---
+++ New Unity Project/Assets/Workshop/Scripts/Touches.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touches : MonoBehaviour {
+    public PicPane mainPane;
+    public GameObject infoPane;
+    public List<ImageModel> allModels;
+
+    // Use this for initialization
+    void Start()
+    {
+        OVRTouchpad.Create();
+        OVRTouchpad.TouchHandler += HandleTouchHandler;
+        allModels = ImageModelDao.Instance.GetImageModels();
+
+        mainPane = GameObject.FindGameObjectWithTag("MainPane").GetComponent(typeof(PicPane)) as PicPane;
+        Debug.Assert(mainPane != null, "mainpane failed, bitch");
+    }
+
+    void HandleTouchHandler (object sender, System.EventArgs e)
+    {
+        OVRTouchpad.TouchArgs touchArgs = (OVRTouchpad.TouchArgs)e;
+        if (touchArgs.TouchType == OVRTouchpad.TouchEvent.SingleTap)
+        {
+            System.Random r = new System.Random();
+            mainPane.setImageModel(allModels[r.Next() % allModels.Count]);
+
+
+            infoPane = GameObject.FindWithTag("InfoPane");
+            mainPane.loadImage();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
