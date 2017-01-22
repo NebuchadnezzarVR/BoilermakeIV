3fe770a7c761fbed9bef34b233bde57b0719bfc5
Aaron Neustedter <aaron97neu@yahoo.com>
2017-01-21 19:26:23 -0500

    added webcam back, deleted unessicary resources

---
+++ New Unity Project/Assets/Workshop/Scripts/webcam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ 
+ public class webcam : MonoBehaviour
+{
+ 
+ 	// Use this for initialization
+ 	void Start()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        webcamTexture.Play();
+        renderer.material.mainTexture = webcamTexture;
+               // gameObject.GetComponent<SpriteRenderer>().material.mainTexture = webcamTexture;
+            }
+ }
+ 	
+ 	/*// Update is called once per frame
+ 	void Update () {
+ 		
+ 	} */
